4e9134723aa761610888ec73cbbbbdc260a90df5
pablopintos <pabbe_spain@hotmail.com>
2022-12-21 15:54:40 +0100

    h2

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.getElementById('navbar');
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+  item.addEventListener('click', e => {
+    // handle clicks on menu items
+  });
+});
+
+window.addEventListener('resize', e => {
+  // toggle the visibility of the navigation bar on small screens
+});
