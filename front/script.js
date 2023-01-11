d3bca080b6fe959a7bcc36c30526ddc54d13650c
obed321 <97287335+obed321@users.noreply.github.com>
2023-01-11 17:56:19 +0100

    Fixat allt

--- front/script.js
+++ front/script.js
@@ -1,24 +1,39 @@
-const navbar = document.getElementById('navbar');
-const navItems = document.querySelectorAll('.nav-item');
 
-navItems.forEach(item => {
-  item.addEventListener('click', e => {
-    // handle clicks on menu items
-  });
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+
+
+searchBtn.addEventListener("click", () => {
+  const searchKey = searchInput.value;
+  searchByName(searchKey);
 });
 
-window.addEventListener('resize', e => {
-  // toggle the visibility of the navigation bar on small screens
-});
 
- function fetchDataNasaImageWithDiscription () {
-  $.ajax({
-    type: "GET",
-    url: "http://localhost:5000/apod",
-    sucess: function(data) {
-    $.each(data, function(index, element) {
-      $("#td-element-" + index).html(element);
-    });
-  }
-});
- }
+document.querySelectorAll('.suggestion').forEach(item => {
+  item.addEventListener('click', event => {
+    //handle click
+    console.log(event.target);
+    searchInput.value = event.target.innerHTML.trim();
+  })
+})
+
+
+async function searchByName(searchKey){
+  await fetch(`http://localhost:8080/nasa/search/${searchKey}`, {method: 'GET'})
+  .then(res => res.json()).then(res => {
+    const items = res.collection.items;
+    console.log(items);
+    items.forEach((item, index) => {
+      const img = item.links[0].href;
+      const title = item.data[0].title
+      const desc = item.data[0].description;
+      jQuery("#imgTable tr:last").after(`<tr>
+      <td>${index+1}</td>
+      <td>${title}</td>  
+      <td><img src=${img}></td>  
+      <td>${desc}</td>  
+      </tr>"
+    `);
+    });  
+  })
+}
