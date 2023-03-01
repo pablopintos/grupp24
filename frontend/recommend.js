b3c1b48147faa05ec72f12dbf18008ae37ee292b
pablopintos <pabbe_spain@hotmail.com>
2023-03-01 14:12:38 +0100

    Fetch api authotize

--- frontend/recommend.js
+++ frontend/recommend.js
@@ -1,60 +1,17 @@
-(function() {
-    
-  function login(callback) {
-      var CLIENT_ID = '38d9e5c35e734857b7e0f633c1fafd99';
-      var REDIRECT_URI = 'http://127.0.0.1:5500/frontend/searched-content.html';
-      function getLoginURL(scopes) {
-          return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
-            '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
-            '&scope=' + encodeURIComponent(scopes.join(' ')) +
-            '&response_type=token';
-      }
-      
-      var url = getLoginURL([
-          'user-read-email'
-      ]);
-      
-      var width = 450,
-          height = 730,
-          left = (screen.width / 2) - (width / 2),
-          top = (screen.height / 2) - (height / 2);
+async function getToken(){
+  // Funktionen hämtar spotifys token och sparar denna lokalt i webbläsaren.
+  let params = (new URL(location.href.replace('#','?'))).searchParams;
+  let token = params.get('access_token');
+  window.localStorage.setItem('access_token',token)   
+  location.href = "listArtists.html";
+}
   
-      window.addEventListener("message", function(event) {
-          var hash = JSON.parse(event.data);
-          if (hash.type == 'access_token') {
-              callback(hash.access_token);
-          }
-      }, false);
-      
-      var w = window.open(url,
-                          'Spotify',
-                          'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                         );
-      
-  }
-
-    function getUserData(accessToken) {
-        return $.ajax({
-            url: 'https://api.spotify.com/v1/me',
-            headers: {
-               'Authorization': 'Bearer ' + accessToken
-            }
-        });
-    }
-
-    var templateSource = document.getElementById('result-template').innerHTML,
-        template = Handlebars.compile(templateSource),
-        resultsPlaceholder = document.getElementById('result'),
-        loginButton = document.getElementById('btn-login');
-
-    loginButton.addEventListener('click', function() {
-        login(function(accessToken) {
-            getUserData(accessToken)
-                .then(function(response) {
-                    loginButton.style.display = 'none';
-                    resultsPlaceholder.innerHTML = template(response);
-                });
-            });
-    });
-
-})();
+function login(){
+  // Funktionen hanterar inloggningen på spotifys konto. 
+  let scope = 'user-follow-read';
+  let clientID = '38d9e5c35e734857b7e0f633c1fafd99';
+  let redirect_uir = 'http://127.0.0.1:5500/frontend/searched-content.html';
+  let response_type = 'token';
+  
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`;
+}
