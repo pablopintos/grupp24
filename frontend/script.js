792e1ada9ab7c845d5004f76a178559d44de2021
keyjpg <93946569+keyjpg@users.noreply.github.com>
2023-03-24 20:05:55 +0100

    bad comit

--- frontend/script.js
+++ frontend/script.js
@@ -2,10 +2,16 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 
+const clientID = '38d9e5c35e734857b7e0f633c1fafd99';
+const clientSecret = 'c3b46ed9a4f04ea2951bbdc0ed54b6f7';
+const redirect_uri = 'http://127.0.0.1:5500/frontend/searched-content.html';
+
 
 searchBtn.addEventListener("click", () => {
   let searchKey = searchInput.value;
   searchByName(searchKey);
+  searchByTrack(searchKey);
+  searchByTrack(searchKey);
 });
 
 searchInput.addEventListener("keyup", function(event) {
@@ -21,8 +27,23 @@ searchInput.addEventListener("keydown", event => {
   }
 });
   
+function login(){  
+  console.log("LOGGING IN ...")
+
+let response_type = '';
+
+async function getToken(){
+  // Funktionen hämtar spotifys token och sparar denna lokalt i webbläsaren.
+  let params = (new URL(location.href.replace('#','?'))).searchParams;
+  let token = params.get('access_token');
+  window.localStorage.setItem('access_token',token)   
+}  
+
 function login(){
   let scope = 'user-follow-read';
+  let redirect_uri = 'http://127.0.0.1:5500/frontend/searched-content.html';
+  let response_type = 'token';
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
   let clientID = '38d9e5c35e734857b7e0f633c1fafd99';
   let redirect_uir = 'http://127.0.0.1:5500/frontend/searched-content.html';
   let response_type = 'token';
