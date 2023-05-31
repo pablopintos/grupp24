f73cb56387e25192ca9b292faa9466b014fcfb68
pablopintos <pabbe_spain@hotmail.com>
2023-05-31 19:21:16 +0200

    Final

--- frontend/script.js
+++ frontend/script.js
@@ -1,33 +1,44 @@
 const clientID = '38d9e5c35e734857b7e0f633c1fafd99';
-const clientSecret = 'c3b46ed9a4f04ea2951bbdc0ed54b6f7';
-const redirect_uri = 'http://127.0.0.1:5500/frontend/searched-content.html';
+const redirectURI = 'http://127.0.0.1:5500/frontend/searched-content.html';
 
 function login() {
-  let scope = 'user-follow-read user-modify-playback-state';
-  let clientID = '38d9e5c35e734857b7e0f633c1fafd99';
-  let redirect_uri = 'http://127.0.0.1:5500/frontend/searched-content.html';
-  let response_type = 'token';
-  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
+  let scope = 'user-read-private user-read-email streaming';
+  let responseType = 'token';
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}&scope=${scope}`;
 }
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = localStorage.getItem('accessToken');
+  const player = new Spotify.Player({
+    name: 'Spotifyspelare',
+    getOAuthToken: cb => { cb(token); }
+  });
+  player.addListener('player_state_changed', state => { console.log(state); });
+  player.addListener('ready', ({ device_id }) => {
+    localStorage.setItem('deviceId', device_id);
+  });
+  player.connect();
+};
 
 window.onload = function() {
   const hash = window.location.hash
-      .substring(1)
-      .split('&')
-      .reduce(function (initial, item) {
-        if (item) {
-          var parts = item.split('=');
-          initial[parts[0]] = decodeURIComponent(parts[1]);
-        }
-        return initial;
-      }, {});
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+      if (item) {
+        var parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
   window.location.hash = '';
 
   if (hash.access_token) {
-    localStorage.setItem('accessToken', hash.access_token); // Store the access token in localStorage
+    localStorage.setItem('accessToken', hash.access_token);
     populateProfile(hash.access_token);
+    fetchAccessToken();
   }
-}
+};
 
 
 /*Föreslagna planeter till sökfältet*/
@@ -36,30 +47,30 @@ document.querySelectorAll('.suggestion').forEach(item => {
     console.log(event.target);
     searchInput.value = event.target.innerHTML.trim();
     searchBtn.click();
-  })
-})
-
+  });
+});
+/* Anrop till backend*/
 async function searchByName(searchKey) {
-  
   await fetch(`http://localhost:8080/search/${searchKey}`, { method: 'GET' })
-    .then(res => res.json()).then(res => {
+    .then(res => res.json())
+    .then(res => {
       const items = res.collection.items.slice(0, 10);
       console.log(items);
       jQuery("#imgTable").empty();
       items.forEach((item, index) => {
         const img = item.links[0].href;
-        const title = item.data[0].title
+        const title = item.data[0].title;
         const desc = item.data[0].description;
-        jQuery("#imgTable").append(`<tr>
-      <td>${index + 1}</td>
-      <td>${title}</td>  
-      <td><img src=${img}></td>  
-      <td>${desc}</td>  
-      </tr>"
-    `);
+        jQuery("#imgTable").append(`
+          <tr>
+            <td>${index + 1}</td>
+            <td>${title}</td>  
+            <td><img src=${img}></td>  
+            <td>${desc}</td>  
+          </tr>
+        `);
       });
-    })
-
+    });
 }
 
 async function searchByTrack(searchKey) {
@@ -81,11 +92,10 @@ async function searchByTrack(searchKey) {
     'Authorization': token
   });
 
-
   const settings = {
     method: 'GET',
     headers: JS_headers
-  }
+  };
 
   try {
     const fetchResponse = await fetch(`http://localhost:8080/track/${searchKey}`, settings);
@@ -93,18 +103,25 @@ async function searchByTrack(searchKey) {
     console.log(data);
 
     const tracksTable = document.getElementById("imgTable2");
-    tracksTable.innerHTML = "";
+    const tracksBody = tracksTable.querySelector("tbody");
+    tracksBody.innerHTML = "";
     const tracks = data.tracks.items;
     tracks.forEach((track, index) => {
       const img = track.album.images[0].url;
       const title = track.name;
       const desc = track.album.name;
-      tracksTable.insertRow().innerHTML = `
+      const uri = track.uri; 
+      const row = document.createElement("tr");
+      row.innerHTML = `
         <td>${index + 1}</td>
         <td>${title}</td>  
         <td><img src=${img}></td>  
         <td>${desc}</td>  
+        <td>
+          <button onclick="playSong('${uri}')">Play</button>
+        </td>
       `;
+      tracksBody.appendChild(row);
     });
 
   } catch (e) {
@@ -113,3 +130,27 @@ async function searchByTrack(searchKey) {
 }
 
 
+
+function playSong(uri) {
+  const accessToken = localStorage.getItem('accessToken');
+  const deviceId = localStorage.getItem('deviceId');
+
+  const playPayload = {
+    uris: [uri]
+  };
+
+  const playSettings = {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(playPayload)
+  };
+
+  const playUrl = `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`;
+
+  fetch(playUrl, playSettings)
+    .then(response => {
+    })
+}
